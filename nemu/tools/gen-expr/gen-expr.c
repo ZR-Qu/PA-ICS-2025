87c0a406f46f0532dbbc393308d509d6a8051ad9
tracer-ics2025 <tracer@njuics.org>
2026-01-20 11:38:44 +0800

    >  compile NEMU 231220000 张三 Linux PA-exp 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:38:44 up 14:35,  1 user,  load average: 0.22, 0.12, 0.04

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -22,6 +22,7 @@
 
 // this should be enough
 static char buf[65536] = {};
+static int buf_pos = 0;
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char *code_format =
 "#include <stdio.h>\n"
@@ -31,8 +32,54 @@ static char *code_format =
 "  return 0; "
 "}";
 
+// 写入单个字符
+static void gen(char c) {
+    if (buf_pos < 65500) { // 缓冲区大小约束
+        buf[buf_pos++] = c;
+    }
+}
+
+// 生成随机数字
+static void gen_num() {
+    buf_pos += sprintf(buf + buf_pos, "%d", rand() % 1000);
+}
+
+// 生成随机运算符
+static void gen_rand_op() {
+    char ops[] = "+-*/";
+    gen(ops[rand() % 4]);
+}
+
+// 生成随机空格
+static void gen_rand_space() {
+    if (rand() % 10 == 0) gen(' ');
+}
+
 static void gen_rand_expr() {
-  buf[0] = '\0';
+    gen_rand_space(); // 空格
+
+    if (buf_pos > 60000) {
+        gen_num();
+        return;
+    }
+
+    switch (rand() % 3) {
+        case 0: 
+            gen_num(); 
+            break;
+        case 1: 
+            gen('('); 
+            gen_rand_expr(); 
+            gen(')'); 
+            break;
+        default: 
+            gen_rand_expr(); 
+            gen_rand_op(); 
+            gen_rand_expr(); 
+            break;
+    }
+    
+    gen_rand_space(); //空格
 }
 
 int main(int argc, char *argv[]) {
@@ -42,9 +89,17 @@ int main(int argc, char *argv[]) {
   if (argc > 1) {
     sscanf(argv[1], "%d", &loop);
   }
+  
   int i;
   for (i = 0; i < loop; i ++) {
+    // 重置指针和缓冲区！
+    buf_pos = 0;
+    buf[0] = '\0'; 
+
     gen_rand_expr();
+
+    // 加上字符串结束符
+    buf[buf_pos] = '\0';
 
     sprintf(code_buf, code_format, buf);
 
@@ -53,7 +108,8 @@ int main(int argc, char *argv[]) {
     fputs(code_buf, fp);
     fclose(fp);
 
-    int ret = system("gcc /tmp/.code.c -o /tmp/.expr");
+    // 2> /dev/null 是为了不让 gcc 的报错刷屏
+    int ret = system("gcc /tmp/.code.c -o /tmp/.expr 2> /dev/null");
     if (ret != 0) continue;
 
     fp = popen("/tmp/.expr", "r");
@@ -62,6 +118,11 @@ int main(int argc, char *argv[]) {
     int result;
     ret = fscanf(fp, "%d", &result);
     pclose(fp);
+
+    // 检查 fscanf 的返回值
+    // 如果 ret != 1，说明程序崩溃了（除0），没读到结果
+    // 坏用例跳过
+    if (ret != 1) continue;
 
     printf("%u %s\n", result, buf);
   }
